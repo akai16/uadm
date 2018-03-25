2c362294fc8f9d69c7ac9b934b71385722bece4d
Akai <gfg2@cin.ufpe.br>
2018-03-25 01:23:59 -0300

    Adicionado GridLayout para os Gifs

--- app/src/main/java/com/example/saint/ultimoaplicativodomundo/fragments/DanielGifsFragment.java
+++ app/src/main/java/com/example/saint/ultimoaplicativodomundo/fragments/DanielGifsFragment.java
@@ -3,11 +3,10 @@ package com.example.saint.ultimoaplicativodomundo.fragments;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -71,7 +70,7 @@ public class DanielGifsFragment extends Fragment {
         this.mViewHolder.mRecyclerView.setAdapter(mGifAdapter);
 
         //3-Definir layout
-        this.mViewHolder.mRecyclerView.setLayoutManager(new LinearLayoutManager(this.context));
+        this.mViewHolder.mRecyclerView.setLayoutManager(new GridLayoutManager(this.context,2));
 
         return view;
     }
